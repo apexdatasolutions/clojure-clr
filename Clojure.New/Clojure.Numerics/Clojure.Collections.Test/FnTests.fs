d6267e5736fcce801235d3b8e664ff983be6f86f
David Miller <dmiller2718@gmail.com>
2021-07-12 14:35:57 -0500

    (Redo) Got the Empty/Cons/Range mocks working

--- Clojure.New/Clojure.Numerics/Clojure.Collections.Test/FnTests.fs
+++ Clojure.New/Clojure.Numerics/Clojure.Collections.Test/FnTests.fs
@@ -5,35 +5,65 @@ open Clojure.Collections
 open Clojure.Fn
 
 
+// I went to this degree of elaboration so I could use this as a general example of implementation of a sequence for tutorial purposes
 
-type SimpleCons(h,t) =
-    let first : obj = h
-    let more : ISeq =  t
+module Util =
 
-    static member seqEquiv (s1:ISeq) (s2:ISeq) =
+    let checkEquals o1 o2 = obj.ReferenceEquals(o1,o2) || o1 <> null && o1.Equals(o2)
+    let rec seqEquals (s1:ISeq) (s2:ISeq) =
         match s1, s2 with   
         | null, null -> true
         | null, _ -> false
         | _, null -> false
-        | _ -> (s1.first() = null && s2.first() = null || s1.first() <> null && s1.first().Equals(s2.first())) && SimpleCons.seqEquiv (s1.next()) (s2.next())
+        | _ ->  checkEquals (s1.first()) (s2.first()) && seqEquals (s1.next()) (s2.next())
+    let seqEquiv s1 s2 = seqEquals s1 s2
+    let seqCount (s:ISeq) = 
+        let rec step (s:ISeq) cnt = if s = null then cnt else step (s.next()) (cnt+1)
+        step s 0
+    let getHashCode (s:ISeq) = 
+        let combine hc x = 31*hc + if x = null then 0 else x.GetHashCode()
+        let rec step (s:ISeq) hc = if s = null then hc else step (s.next()) (combine hc (s.first()))
+        step s 1
+    let rec seqToString (s:ISeq) = 
+        let itemToString (o:obj) =
+            match o with
+            | :? Seqable as s -> seqToString (s.seq())
+            | _ -> o.ToString()
+        let rec itemsToString (s:ISeq) =
+            if s = null then "" else (itemToString (s.first())) + (itemsToString (s.next()))  
+        if s = null then "nil" else "(" + (itemsToString s) + ")"        
 
+type SimpleCons(h,t) =
+    let head : obj = h      // I had to restrain myself from calling these car & cdr
+    let tail : ISeq =  t
+ 
     interface ISeq with
-        member x.first() = first
+        member x.first() = head
         member x.next() = (x:>ISeq).more().seq()
-        member x.more() = if more = null then (SimpleEmptySeq() :> ISeq) else more
+        member x.more() =  if tail <> null then tail else (SimpleEmptySeq() :> ISeq)
         member x.cons(o) = SimpleCons(o,x) :> ISeq
 
     interface IPersistentCollection with
-        member x.count() = 1+ (if more = null then 0 else more.count())
-        member x.cons(o) = SimpleCons(o,x) :> IPersistentCollection
+        member x.count() = 1 + Util.seqCount tail
+        member x.cons(o) = (x:>ISeq).cons(o) :> IPersistentCollection
         member _.empty() = SimpleEmptySeq() :> IPersistentCollection
         member x.equiv(o) =
             match o with
-            | :? Seqable as s -> SimpleCons.seqEquiv (x:>ISeq) (s.seq())
+            | :? Seqable as s -> Util.seqEquiv (x:>ISeq) (s.seq())
             | _ -> false
 
     interface Seqable with
         member x.seq() = (x:>ISeq)
+
+    override x.Equals(o) = 
+        match o with
+        | :? Seqable as s -> Util.seqEquals (x:>ISeq) (s.seq())
+        | _ -> false
+
+    override x.GetHashCode() = Util.getHashCode x
+
+    override x.ToString() = Util.seqToString x
+        
    
 and  SimpleEmptySeq() =
 
@@ -45,42 +75,56 @@ and  SimpleEmptySeq() =
 
     interface IPersistentCollection with
         member _.count() = 0
-        member x.cons(o) = SimpleCons(o,x) :> IPersistentCollection
+        member x.cons(o) = (x:>ISeq).cons(o) :> IPersistentCollection
         member x.empty() = x:>IPersistentCollection
-        member x.equiv(o) = 
-            match o with
-            | :? Seqable as s -> SimpleCons.seqEquiv (x:>ISeq) (s.seq())
-            | _ -> false
+        member x.equiv(o) = x.Equals(o)
 
     interface Seqable with
-        member x.seq() = (x:>ISeq)
+        member x.seq() = null
+
+    override x.Equals(o) = 
+        match o with
+        | :? Seqable as s -> s.seq() = null
+        | _ -> false
+
+    override x.GetHashCode() = 1
        
+    override x.ToString() = "()"
 
 // Make a super-simple Range sequence to implement ISeq
 [<AllowNullLiteral>]
 type SimpleRange(s,e) =
     let startVal : int = s
-    let endVal : int = s
+    let endVal : int = e
 
     interface ISeq with
         member x.first() = upcast startVal 
-        member x.next() = if startVal = endVal  then null else SimpleRange(startVal+1,endVal):>ISeq
-        member x.more() = 
-            let s = (x:>ISeq).next()
-            if s = null then (SimpleEmptySeq():>ISeq) else s
+        member x.next() = (x:>ISeq).more().seq()
+        member x.more() = if startVal = endVal then SimpleEmptySeq() :> ISeq else SimpleRange(startVal+1,endVal) :> ISeq
         member x.cons(o) = SimpleCons(o,(x:>ISeq)) :> ISeq
 
     interface IPersistentCollection with
         member _.count() = endVal-startVal+1
-        member x.cons(o) =  SimpleCons(o,(x:>ISeq)) :> IPersistentCollection
+        member x.cons(o) =  (x:>ISeq).cons(o) :> IPersistentCollection
         member _.empty() = SimpleEmptySeq() :> IPersistentCollection
         member x.equiv(o) = 
             match o with
-            | :? Seqable as s -> SimpleCons.seqEquiv (x:>ISeq) (s.seq())
+            | :? Seqable as s -> Util.seqEquiv (x:>ISeq) (s.seq())
             | _ -> false
 
     interface Seqable with
         member x.seq() = (x:>ISeq)
+
+    override x.Equals(o) = 
+        match o with
+        | :? Seqable as s -> Util.seqEquals (x:>ISeq) (s.seq())
+        | _ -> false
+
+    override x.GetHashCode() = Util.getHashCode x
+
+    override x.ToString() = Util.seqToString x
+        
+
 
 [<Tests>]
 let emptySeqTests = 
@@ -94,7 +138,7 @@ let emptySeqTests =
             
             let c = es.cons(1)
             Expect.equal (c.first()) (upcast 1) "emptyseq.cons.first should be new item"
-            Expect.equal (c.next()) es "emptyseq.cons.next should be itself"
+            Expect.isNull (c.next()) "emptyseq.cons.next should be null"
 
 
         testCase "emptyseq implements IPersistentCollection" <| fun _ ->
@@ -106,7 +150,7 @@ let emptySeqTests =
 
         testCase "emptyseq implements Seqable" <| fun _ ->
              let es = SimpleEmptySeq() 
-             Expect.equal ((es:>Seqable).seq()) (upcast es) "emptyseq.seq should be itself"
+             Expect.isNull ((es:>Seqable).seq()) "emptyseq.seq should be null"
         
     ]
 
@@ -141,39 +185,65 @@ let consTests =
 
         testCase "cons implements Seqable" <| fun _ ->
              let c = makeConsSeq 4 
+             Expect.equal ((c:>Seqable).seq()) c "cons.seq should be itself"
+        
+    ]
+
+
+[<Tests>]
+let rangeTests = 
+    testList "ranges" [
+        
+        testCase "SimpleRange implements ISeq" <| fun _ ->
+            let c = SimpleRange(0,3) :> ISeq
+            let mutable mc = c
+            for i = 0 to 3 do
+                Expect.equal (mc.first()) (upcast i) "ith element of this range should be i"
+                mc <- mc.next()
+            
+            let c2 = c.cons(10)
+            Expect.equal (c2.first()) (upcast 10) "range.cons.first should be new item"
+            Expect.equal (c2.next()) c "range.cons.next should be itself"
+
+
+        testCase "SimpleRange implements IPersistentCollection" <| fun _ ->
+            let c = SimpleRange(0,3)
+            let pc = c :> IPersistentCollection
+            Expect.equal (pc.count()) 4 "range.count should be item count"
+            Expect.equal (pc.empty().GetType()) typeof<SimpleEmptySeq> "range.empty should be an emptySeq"
+            Expect.isTrue (pc.equiv(c)) "range should be equiv of itself"
+            Expect.isFalse (pc.equiv((c:>ISeq).next()))  "range should not be equiv of its next"
+
+        testCase "SimpleRange implements Seqable" <| fun _ ->
+             let c = SimpleRange(0,3)
              Expect.equal ((c:>Seqable).seq()) (upcast c) "cons.seq should be itself"
         
     ]
 
-//[<Tests>]
-//let tests =
-//  testList "samples" [
-//    testCase "universe exists (╭ರᴥ•́)" <| fun _ ->
-//      let subject = true
-//      Expect.isTrue subject "I compute, therefore I am."
 
-//    testCase "when true is not (should fail)" <| fun _ ->
-//      let subject = false
-//      Expect.isTrue subject "I should fail because the subject is false"
+[<Tests>]
+let boundedLengthTests = 
+    testList "boundedLength" [
 
-//    testCase "I'm skipped (should skip)" <| fun _ ->
-//      Tests.skiptest "Yup, waiting for a sunny day..."
+        testCase "boundedLength on range, limit hit" <| fun _ ->
+            let r = SimpleRange(0,9) :> ISeq
+            Expect.equal (AFn.boundedLength(r,5)) 6 "Over limit should be bound+1"
 
-//    testCase "I'm always fail (should fail)" <| fun _ ->
-//      Tests.failtest "This was expected..."
+        testCase "boundedLength on conses, limit hit" <| fun _ ->
+            let c = makeConsSeq 9 
+            Expect.equal (AFn.boundedLength(c,5)) 6 "Over limit should be bound+1"    
 
-//    testCase "contains things" <| fun _ ->
-//      Expect.containsAll [| 2; 3; 4 |] [| 2; 4 |]
-//                         "This is the case; {2,3,4} contains {2,4}"
+        testCase "boundedLength on range, seq end hit" <| fun _ ->
+            let r = SimpleRange(0,9) :> ISeq
+            let n = r.count()
+            let m = AFn.boundedLength(r,20)
+            Expect.equal (AFn.boundedLength(r,20)) (r.count()) "under limit on range should be seq count"
 
-//    testCase "contains things (should fail)" <| fun _ ->
-//      Expect.containsAll [| 2; 3; 4 |] [| 2; 4; 1 |]
-//                         "Expecting we have one (1) in there"
-
-//    testCase "Sometimes I want to ༼ノಠل͟ಠ༽ノ ︵ ┻━┻" <| fun _ ->
-//      Expect.equal "abcdëf" "abcdef" "These should equal"
-
-//    test "I am (should fail)" {
-//      "╰〳 ಠ 益 ಠೃ 〵╯" |> Expect.equal true false
-//    }
-//  ]
+        testCase "boundedLength on conses, seq end hit" <| fun _ ->
+            let c = makeConsSeq 2
+            let n = c.count()
+            let m = AFn.boundedLength(c,20)
+            Expect.equal (AFn.boundedLength(c,20)) (c.count()) "under limit on conses should be seq count"    
+    
+    
+    ]

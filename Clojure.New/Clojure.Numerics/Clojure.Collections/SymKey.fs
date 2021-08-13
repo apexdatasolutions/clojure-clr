3a48f59111657067a6eda31586b6a76fd07b6062
David Miller <dmiller2718@gmail.com>
2021-08-13 15:13:54 -0500

    (Redo) Fix some code formatting and move to this. references in Collections, part 3

--- Clojure.New/Clojure.Numerics/Clojure.Collections/SymKey.fs
+++ Clojure.New/Clojure.Numerics/Clojure.Collections/SymKey.fs
@@ -3,41 +3,35 @@
 open System
 open System.Collections.Concurrent
 
-type Symbol private (meta: IPersistentMap, internedNS: string, internedName: string) = 
-    
-    // from the name, you would think that the strings for namespace and name were to be interned strings.
-    // Check the ClojureJVM code.
-
+type Symbol private (meta: IPersistentMap, ns: string, name: string) = 
     inherit AFn()
 
-    let ns = internedNS
-    let name = internedName
-    let meta = meta
-    let mutable hasheq = 0
+    let mutable hasheq : int option = None
 
     [<NonSerialized>]
     let mutable toStringCached : string option = None
 
-    private new(ns:string,name:string) = Symbol(null,ns,name)
+    private new(ns,name) = Symbol(null,ns,name)
 
     member x.Name = name
     member x.NS = ns
 
 
-    static member intern(ns:string, name:string) : Symbol = Symbol(ns,name)
-    static member intern(nsname:string) : Symbol = 
+    static member intern(ns, name) = Symbol(ns,name)
+    static member intern(nsname: string) = 
         let i = nsname.IndexOf('/')
-        if i = -1 || nsname.Equals("/") 
-        then Symbol(null,nsname) 
-        else Symbol(nsname.Substring(0,i),nsname.Substring(i+1))
+        if i = -1 || nsname.Equals("/") then 
+            Symbol(null,nsname) 
+        else 
+            Symbol(nsname.Substring(0,i),nsname.Substring(i+1))
 
     // JVM comment: the create thunks preserve binary compatibility with code compiled
     // JVM comment: against earlier version of Clojure and can be removed (at some point).
     // So I'm leaving them out.
-    static member create(ns:string, name:string) : Symbol = Symbol.intern(ns,name)
-    static member create(nsname:string) : Symbol = Symbol.intern(nsname)
+    static member create(ns, name) : Symbol = Symbol.intern(ns,name)
+    static member create(nsname) : Symbol = Symbol.intern(nsname)
 
-    override x.ToString() =
+    override _.ToString() =
         match toStringCached with  
         | Some s -> s
         | None  -> 
@@ -45,48 +39,54 @@ type Symbol private (meta: IPersistentMap, internedNS: string, internedName: str
             toStringCached <- Some s
             s
 
-    override x.Equals(o:obj) : bool =
+    override this.Equals(o:obj) : bool =
         match o with
-        | _ when Object.ReferenceEquals(x,o) -> true
+        | _ when (this :> obj) = o -> true
         | :? Symbol as s ->  Util.equals(ns,s.NS) && name.Equals(s.Name)
         | _ -> false
 
-    override x.GetHashCode() = Util.hashCombine(name.GetHashCode(),Util.hash(ns))
+    override _.GetHashCode() = Util.hashCombine(name.GetHashCode(),Util.hash(ns))
 
     interface IHashEq with  
-        member x.hasheq() =
-            if hasheq = 0 then hasheq <- Util.hashCombine(Murmur3.HashString(name),Util.hash(ns))
-            hasheq
+        member _.hasheq() =
+            match hasheq with
+            | None ->
+                let h = Util.hashCombine(Murmur3.HashString(name),Util.hash(ns))
+                hasheq <- Some h
+                h
+            | Some h -> h
     
     interface IMeta with    
-        member x.meta() = meta
+        member _.meta() = meta
 
     interface IObj with
-        member x.withMeta(m:IPersistentMap) = if Object.ReferenceEquals(meta,m) then upcast x else upcast Symbol(m,ns,name)
+        member this.withMeta(m:IPersistentMap) = if meta = m then upcast this else upcast Symbol(m,ns,name)
 
     interface Named with
         member x.getNamespace() = ns
         member x.getName() = name
 
     interface IFn with
-        member x.invoke(o:obj) : obj = RT.get(obj,x)
-        member x.invoke(o:obj, notFound,obj) : obj = RT.get3(obj,x,notFound)
+        member this.invoke(o) = RT.get(o,this)
+        member this.invoke(o, notFound) = RT.get3(o,this,notFound)
 
     interface IComparable<Symbol> with
-        member x.CompareTo(s:Symbol) : int = 
-            if x.Equals(s) then 0
+        member this.CompareTo(s) = 
+            if this.Equals(s) then 0
             elif isNull ns && not (isNull s.NS) then -1
             elif not (isNull ns) then
-                if isNull s.NS then 1
+                if isNull s.NS then 
+                    1
                 else 
                     let nsc = ns.CompareTo(s.NS)
                     if nsc <> 0 then nsc else name.CompareTo(s.Name)
-            else name.CompareTo(s.Name)
+            else 
+                name.CompareTo(s.Name)
 
     interface IComparable with
-        member x.CompareTo(o:obj) =
+        member this.CompareTo(o) =
             match o with
-            | :? Symbol as s -> (x:>IComparable<Symbol>).CompareTo(s)
+            | :? Symbol as s -> (this:>IComparable<Symbol>).CompareTo(s)
             | _ -> invalidArg "o" "must compare to a non-null Symbol"
 
 
@@ -101,10 +101,10 @@ type Symbol private (meta: IPersistentMap, internedNS: string, internedName: str
 
 
 [<Sealed>][<AllowNullLiteral>]
-type Keyword private (s:Symbol) =
+type Keyword private (sym:Symbol) =
     inherit AFn()
-    let sym = s
-    let hasheq = (s:>IHashEq).hasheq() + 0x9e3779b9
+
+    let hasheq = (sym:>IHashEq).hasheq() + 0x9e3779b9
 
     [<NonSerialized>]
     let mutable toStringCached : String option = None
@@ -114,14 +114,15 @@ type Keyword private (s:Symbol) =
     // map from symbols to keywords to uniquify keywords
     static member symKeyMap = ConcurrentDictionary<Symbol,WeakReference<Keyword>>()
 
-    static member intern(s: Symbol) : Keyword =
-        let useSym s:Symbol = if  (s:>IMeta).meta() |> isNull  then s else downcast (s:>IObj).withMeta(null) 
+    static member intern(s) =
+        let useSym s = if  (s:>IMeta).meta() |> isNull  then s else downcast (s:>IObj).withMeta(null) 
 
         let exists, wref = Keyword.symKeyMap.TryGetValue(s)
         if exists 
         then
             let exists, existingKw = wref.TryGetTarget()
-            if exists then existingKw
+            if exists then 
+                existingKw
             else 
                 // WeakReference timed out.  Set a new Keyword in place
                 // we don't have a timing problem here.  if someone managed to sneak another keyword in here, it won't really matter.
@@ -138,7 +139,7 @@ type Keyword private (s:Symbol) =
     static member intern(ns,name) = Keyword.intern(Symbol.intern(ns,name))
     static member intern(nsname) = Keyword.intern(Symbol.intern(nsname))
 
-    override x.ToString() =
+    override _.ToString() =
         match toStringCached with
         | Some s -> s
         | None ->
@@ -147,60 +148,60 @@ type Keyword private (s:Symbol) =
             s
         
     interface IEquatable<Keyword> with 
-        member x.Equals(k:Keyword) : bool = Object.ReferenceEquals(x,k) || sym.Equals(k.Symbol)
+        member this.Equals(k) = this = k || sym.Equals(k.Symbol)
 
-    override x.Equals(o:obj) : bool = 
+    override this.Equals(o) = 
         match o with
-        | :? Keyword as k -> (x:>IEquatable<Keyword>).Equals(k)
+        | :? Keyword as k -> (this:>IEquatable<Keyword>).Equals(k)
         | _ -> false
 
-    override x.GetHashCode() = sym.GetHashCode() + 0x9e3779b9
+    override _.GetHashCode() = sym.GetHashCode() + 0x9e3779b9
 
     interface IHashEq with  
-        member x.hasheq() = hasheq
+        member _.hasheq() = hasheq
 
     // I prefer these for myself.
-    member x.NS = sym.NS
-    member x.Name = sym.Name
+    member _.NS = sym.NS
+    member _.Name = sym.Name
 
     interface Named with
-        member x.getNamespace() = sym.NS
-        member x.getName() = sym.Name
+        member _.getNamespace() = sym.NS
+        member _.getName() = sym.Name
 
     interface IFn with
         // (:keyword arg) = (gat arg :keyword)
-        member x.invoke(arg1:obj) = 
+        member this.invoke(arg1) = 
             match arg1 with
-            | :? ILookup as ilu -> ilu.valAt(x)
-            | _ -> RT.get(arg1,x)
+            | :? ILookup as ilu -> ilu.valAt(this)
+            | _ -> RT.get(arg1,this)
         // (:keyword arg default) = (gat arg :keyword default)
-        member x.invoke(arg1:obj, notFound:obj) = 
+        member this.invoke(arg1, notFound) = 
             match arg1 with
-            | :? ILookup as ilu -> ilu.valAt(x,notFound)
-            | _ -> RT.get3(arg1,x,notFound)
+            | :? ILookup as ilu -> ilu.valAt(this,notFound)
+            | _ -> RT.get3(arg1,this,notFound)
 
     interface IComparable<Keyword> with
-        member x.CompareTo(k:Keyword) = (sym:>IComparable<Symbol>).CompareTo(k.Symbol)
+        member _.CompareTo(k) = (sym:>IComparable<Symbol>).CompareTo(k.Symbol)
 
     interface IComparable with  
-        member x.CompareTo(o:obj) =
+        member this.CompareTo(o) =
             match o with 
-            | :? Keyword as k -> (x:>IComparable<Keyword>).CompareTo(k)
+            | :? Keyword as k -> (this:>IComparable<Keyword>).CompareTo(k)
             | _ -> invalidArg "0" "Cannot campre to null or non-Keyword"
 
     // we had operator overloads for ==, !=, <, >
     // TODO: do we need?
 
-    static member find(s:Symbol) : Keyword =
+    static member find(s) =
         let exists, wr = Keyword.symKeyMap.TryGetValue(s)
-        if exists 
-        then
+        if exists then
             let exists, kw = wr.TryGetTarget()
             if exists then kw else null
-        else null
+        else 
+            null
 
-    static member find(ns:string, name:string) : Keyword = Keyword.find(Symbol.intern(ns,name))
-    static member find(nsname:string) : Keyword = Keyword.find(Symbol.intern(nsname))
+    static member find(ns, name) = Keyword.find(Symbol.intern(ns,name))
+    static member find(nsname) = Keyword.find(Symbol.intern(nsname))
  
     // not done yet
     //       public void GetObjectData(SerializationInfo info, StreamingContext context)
